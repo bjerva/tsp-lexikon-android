4a3dc32e842957e3bab4304576cea5a29ae59312
Johannes Bjerva <johannesbjerva@gmail.com>
2012-12-21 17:39:04 +0100

    Implemented click listener in sign list

--- src/com/bjerva/tsplex/SignListFragment.java
+++ src/com/bjerva/tsplex/SignListFragment.java
@@ -3,10 +3,13 @@ package com.bjerva.tsplex;
 import java.util.ArrayList;
 
 import android.os.Bundle;
+import android.support.v4.app.FragmentTransaction;
 import android.support.v4.app.ListFragment;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
+import android.widget.AdapterView;
+import android.widget.AdapterView.OnItemClickListener;
 import android.widget.ArrayAdapter;
 
 public class SignListFragment extends ListFragment {
@@ -30,7 +33,22 @@ public class SignListFragment extends ListFragment {
 			adapterItems.add(sign.words[0].word);
 		}
 		
+		//Create and set adapter
 		ArrayAdapter<String> mAdapter = new ArrayAdapter<String>(ma, android.R.layout.simple_list_item_1, adapterItems);
 		getListView().setAdapter(mAdapter);
+		
+		//Set listener
+		getListView().setOnItemClickListener(new OnItemClickListener() {
+			public void onItemClick(AdapterView<?> parent, View view, int position, long id){
+				//Create detail fragment
+				SignDetailFragment newFragment = new SignDetailFragment();
+				
+				//Add to container
+				FragmentTransaction transaction = getActivity().getSupportFragmentManager().beginTransaction();
+				transaction.replace(R.id.fragment_container, newFragment);
+				transaction.addToBackStack(null);
+				transaction.commit();
+			}
+		});
 	}
 }
