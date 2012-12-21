f12f9ef53bf4a2b32f5db6c1992e22552b0ef7b9
Johannes Bjerva <johannesbjerva@gmail.com>
2012-12-21 17:34:21 +0100

    Corrected bugs in the xml

--- src/com/bjerva/tsplex/MainActivity.java
+++ src/com/bjerva/tsplex/MainActivity.java
@@ -4,7 +4,6 @@ import org.json.JSONArray;
 
 import android.app.ProgressDialog;
 import android.os.Bundle;
-import android.support.v4.app.Fragment;
 import android.support.v4.app.FragmentActivity;
 import android.view.Menu;
 
@@ -20,7 +19,7 @@ public class MainActivity extends FragmentActivity {
 		setContentView(R.layout.activity_sign_listing);
 
 		if (savedInstanceState == null){
-			Fragment newFragment = new SignListFragment();
+			SignListFragment newFragment = new SignListFragment();
 			getSupportFragmentManager().beginTransaction().add(
 					R.id.fragment_container, newFragment).commit();
 		}
