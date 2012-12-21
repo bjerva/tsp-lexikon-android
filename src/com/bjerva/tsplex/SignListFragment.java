abc9f9675aafa4fb0cadbcc23131f0ce7e41feb4
Johannes Bjerva <johannesbjerva@gmail.com>
2012-12-21 17:28:45 +0100

    Loading signs into list view

--- src/com/bjerva/tsplex/SignListFragment.java
+++ src/com/bjerva/tsplex/SignListFragment.java
@@ -1,7 +1,36 @@
 package com.bjerva.tsplex;
 
+import java.util.ArrayList;
+
+import android.os.Bundle;
 import android.support.v4.app.ListFragment;
+import android.view.LayoutInflater;
+import android.view.View;
+import android.view.ViewGroup;
+import android.widget.ArrayAdapter;
 
 public class SignListFragment extends ListFragment {
 
+	private View myView;
+	
+	@Override
+	public View onCreateView(LayoutInflater inflater, ViewGroup container,
+			Bundle savedInstanceState){
+		myView = inflater.inflate(R.layout.sign_list_fragment, container, false);
+		return myView;
+	}
+	
+	@Override
+	public void onActivityCreated(Bundle savedInstanceState){
+		super.onActivityCreated(savedInstanceState);
+		MainActivity ma = (MainActivity) getActivity();
+		
+		ArrayList<String> adapterItems = new ArrayList<String>();
+		for(SignModel sign: ma.signs){
+			adapterItems.add(sign.words[0].word);
+		}
+		
+		ArrayAdapter<String> mAdapter = new ArrayAdapter<String>(ma, android.R.layout.simple_list_item_1, adapterItems);
+		getListView().setAdapter(mAdapter);
+	}
 }
