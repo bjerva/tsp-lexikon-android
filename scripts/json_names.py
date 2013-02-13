e326699972b9cc5d9dbc66a916732f113f925bc7
Johannes Bjerva <johannesbjerva@gmail.com>
2013-02-13 10:07:58 +0100

    Updated the scripts

--- scripts/json_names.py
+++ scripts/json_names.py
@@ -1,22 +1,25 @@
 #!/usr/bin/env python
 # encoding: utf-8
 
-import json, io
-from pprint import pprint
+import json
 
+if __name__ == '__main__':
+	f = open('signs2.json')
 
-f = open("signs2.json")
+	data = json.load(f)
+	outfile = open(u'words2.json', 'w')
 
-data = json.load(f)
-outfile = open(u'words2.json', "w")
+	l = []
+	for entry in data:
+		#Unpack the word-arrays
+		strbuf = ''
+		for word in entry['words']:
+			strbuf += word['word']+', '
+		strbuf = strbuf[:-2]
+		#Add a dict with word and id to l
+		l.append({'word':strbuf, 'id':entry['id']})
 
-l = []
-for entry in data:
-	strbuf = ""
-	for word in entry["words"]:
-		strbuf += word["word"]+", "
-	strbuf = strbuf[:-2]
-	l.append({"word":strbuf, "id":entry["id"]})
-json.dump(l, outfile)
-outfile.close()
-f.close()
+	#Dump the list as a json array
+	json.dump(l, outfile)
+	outfile.close()
+	f.close()
--- scripts/split_json.py
+++ scripts/split_json.py
@@ -1,21 +1,22 @@
 #!/usr/bin/env python
 # encoding: utf-8
 
-import json, io
-from pprint import pprint
+import json
 
+if __name__ == '__main__':
+	f = open('../assets/signs2.json')
 
-f = open("../assets/signs2.json")
+	data = json.load(f)
 
-data = json.load(f)
+	#Split each json object into a new file
+	for entry in data:
+		outfile = open(unicode(str(entry['id'])+'.json'), 'w')
+		json.dump(entry, outfile)
+		outfile.close()
 
-for entry in data:
-	outfile = open(unicode(str(entry["id"])+".json"), "w")
-	json.dump(entry, outfile)
-	outfile.close()
-
-for entry in data:
-	small_f = open(str(entry["id"])+".json")
-	small_d = json.load(small_f)
-	small_f.close()
-	assert small_d == entry
+	#Assert everything went well
+	for entry in data:
+		small_f = open(str(entry['id'])+'.json')
+		small_d = json.load(small_f)
+		small_f.close()
+		assert small_d == entry
